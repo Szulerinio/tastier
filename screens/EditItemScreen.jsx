6735a22692134e3319889c0478e0683de437ff53
Szulerinio <szymonkozlowski48@gmail.com>
2022-02-12 17:51:58 +0100

    Add save icon in EditItemScreen

--- screens/EditItemScreen.jsx
+++ screens/EditItemScreen.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
-import { TextInput, View } from "react-native";
+import { TextInput, View, TouchableOpacity, Image } from "react-native";
 import LabeledTextInput from "../components/LabeledTextInput";
+import React from "react";
 
 const EditItemScreen = ({ route, navigation }) => {
   const { code, type, brand, name, rate } = route.params; // TODO add functioning save button
-  const [enteredCode, setEnteredCode] = useState();
-  const [enteredType, setEnteredType] = useState();
-  const [enteredBrand, setEnteredBrand] = useState();
-  const [enteredName, setEnteredName] = useState();
-  const [enteredRate, setEnteredRate] = useState();
+
+  const [enteredCode, setEnteredCode] = useState(code);
+  const [enteredType, setEnteredType] = useState(type);
+  const [enteredBrand, setEnteredBrand] = useState(brand);
+  const [enteredName, setEnteredName] = useState(name);
+  const [enteredRate, setEnteredRate] = useState(rate.toString());
   const handleCodeChange = (event) => {
     setEnteredCode(event);
   };
@@ -24,35 +26,59 @@ const EditItemScreen = ({ route, navigation }) => {
   const handleRateChange = (event) => {
     setEnteredRate(event);
   };
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate("Edit", {
+              enteredCode,
+              type,
+              brand,
+              name,
+              rate,
+            })
+          }
+        >
+          <Image
+            style={{ width: 30, height: 30, marginRight: 10 }}
+            source={require("../assets/icons8-save-30.png")}
+          />
+        </TouchableOpacity>
+      ),
+    });
+  });
+
   return (
     <View>
       <LabeledTextInput
         key={0}
-        value={code}
+        value={enteredCode}
         label="code"
         onChange={handleCodeChange}
       ></LabeledTextInput>
       <LabeledTextInput
         key={1}
-        value={type}
+        value={enteredType}
         label="type"
         onChange={handleTypeChange}
       ></LabeledTextInput>
       <LabeledTextInput
         key={2}
-        value={brand}
+        value={enteredBrand}
         label="brand"
         onChange={handleBrandChange}
       ></LabeledTextInput>
       <LabeledTextInput
         key={3}
-        value={name}
+        value={enteredName}
         label="name"
         onChange={handleNameChange}
       ></LabeledTextInput>
       <LabeledTextInput
         key={4}
-        value={rate.toString()}
+        value={enteredRate.toString()}
         label="rate"
         onChange={handleRateChange}
       ></LabeledTextInput>
