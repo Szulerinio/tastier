fcc593eed4e3061943d26308b9da8e39b4026158
Szulerinio <szymonkozlowski48@gmail.com>
2022-02-21 14:10:20 +0100

    Change navigation after scanning existing item

--- screens/ScannerScreen.jsx
+++ screens/ScannerScreen.jsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
+import { useContext } from "react";
 import BarCodeScannerElemet from "../components/BarCodeScannerElemet";
-
+import DataContext from "../context/data-context";
 const ScanerScreen = ({ route, navigation }) => {
+  const ctx = useContext(DataContext);
   const scanHandler = (code) => {
     console.log(code);
-    navigation.navigate("Edit", { code });
+    if (ctx.items.find((item) => item.code == code)) {
+      navigation.navigate("Item", { code });
+    } else navigation.navigate("Edit", { code });
   };
   return <BarCodeScannerElemet onScan={scanHandler}></BarCodeScannerElemet>;
 };
