faee48934cfd8a213ebc5f661abee2873b701f97
Szulerinio <szymonkozlowski48@gmail.com>
2022-03-17 23:07:20 +0100

    Change navigation to remount scanner (bug bypass)

--- screens/ScannerScreen.jsx
+++ screens/ScannerScreen.jsx
@@ -2,12 +2,31 @@ import { useContext } from "react";
 import BarCodeScannerElemet from "../components/BarCodeScannerElemet";
 import DataContext from "../context/data-context";
 const ScanerScreen = ({ route, navigation }) => {
+  console.log("SCANNER SCREEN");
   const ctx = useContext(DataContext);
   const scanHandler = (code) => {
     console.log(code);
     if (ctx.items.find((item) => item.code == code)) {
-      navigation.navigate("Item", { code });
-    } else navigation.navigate("Edit", { code });
+      const routes = [
+        { name: "Home", params: undefined },
+
+        { name: "Item", params: { code: code } },
+      ];
+      navigation.reset({
+        index: 1,
+        routes: routes,
+      });
+    } else {
+      const routes = [
+        { name: "Home", params: undefined },
+
+        { name: "Edit", params: { code: code } },
+      ];
+      navigation.reset({
+        index: 1,
+        routes: routes,
+      });
+    }
   };
   return <BarCodeScannerElemet onScan={scanHandler}></BarCodeScannerElemet>;
 };
