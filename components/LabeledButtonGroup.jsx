8d65fefd616b382ab47d86379bc704830ccb83fa
Szulerinio <szymonkozlowski48@gmail.com>
2022-04-06 22:29:11 +0200

    change colors

--- components/LabeledButtonGroup.jsx
+++ components/LabeledButtonGroup.jsx
@@ -3,7 +3,12 @@ import { ButtonGroup } from "react-native-elements";
 import { Text } from "react-native";
 import labeledComponentStyle from "./styles/labeledComponentStyle";
 
+import { useTheme } from '@react-navigation/native';
+
+
 function LabeledButtonGroup(props) {
+  
+  const { colors } = useTheme();
   const { label, onChange, selectedIndexes, selectMultiple } = props;
   const changeHandler = (event) => {
     onChange(event);
@@ -11,6 +16,8 @@ function LabeledButtonGroup(props) {
 
   const buttons = selectMultiple ? (
     <ButtonGroup
+    selectedButtonStyle={{backgroundColor: colors.primary}}
+    buttonStyle={{backgroundColor:colors.card}}
       buttons={["0", "1", "2", "3", "4", "5"]}
       selectedIndexes={selectedIndexes}
       selectMultiple
@@ -21,6 +28,8 @@ function LabeledButtonGroup(props) {
     />
   ) : (
     <ButtonGroup
+    selectedButtonStyle={{backgroundColor: colors.primary}}
+    buttonStyle={{backgroundColor:colors.card}}
       buttons={["0", "1", "2", "3", "4", "5"]}
       selectedIndex={selectedIndexes}
       onPress={(value) => {
