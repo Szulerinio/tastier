8765d24a4b34a5c941cff8d8872a3d0520955041
Szulerinio <szymonkozlowski48@gmail.com>
2022-04-29 16:14:25 +0200

    Add flashlight

--- components/CameraElement.jsx
+++ components/CameraElement.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import { Camera } from "expo-camera";
+import ButtonPrimary from "./ButtonPrimary";
 
 export default function CameraElemet(props) {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
+  const [isTorchOn, setIsTorchOn] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -17,19 +19,31 @@ export default function CameraElemet(props) {
     setScanned(true);
     props.onScan(data);
   };
+  const handleTorchButtonClick = () => {
+    console.log(isTorchOn);
+    setIsTorchOn(!isTorchOn);
+  };
   if (!hasPermission) {
     return <Text>No access to camera</Text>;
   }
 
   return (
     <View style={styles.container}>
+      {/* <View style={StyleSheet.absoluteFillObject}> */}
       <View style={{ aspectRatio: 3 / 4, width: "100%" }}>
         <Camera
           style={{ flex: 1 }}
           type={Camera.Constants.Type.back}
           ratio="4:3"
           onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          flashMode={isTorchOn ? "torch" : "off"}
         ></Camera>
+        <ButtonPrimary
+          buttonProps={{
+            onPress: handleTorchButtonClick,
+          }}
+          title={"Toggle flashlight"}
+        ></ButtonPrimary>
       </View>
     </View>
   );
