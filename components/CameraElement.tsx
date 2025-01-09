9c0130a429a49448e2b84d333480754a0a788cb9
Szulerinio <szymonkozlowski48@gmail.com>
2025-01-09 23:03:50 +0100

    fix camera element compoent and move it to typescript

---
+++ components/CameraElement.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { Button } from "react-native";
+import { CameraView, CameraType, useCameraPermissions } from "expo-camera";
+
+import ButtonPrimary from "./ButtonPrimary";
+
+export default function CameraElemet(props) {
+  const [permission, requestPermission] = useCameraPermissions();
+  const [scanned, setScanned] = useState(false);
+  const [isTorchOn, setIsTorchOn] = useState(false);
+  const facing: CameraType = "back";
+
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    props.onScan(data);
+    console.log("type", type);
+    console.log("data", data);
+  };
+  const handleTorchButtonClick = () => {
+    console.log(isTorchOn);
+    setIsTorchOn(!isTorchOn);
+  };
+  return (
+    <View style={styles.container}>
+      <View style={{ aspectRatio: 3 / 4, width: "100%" }}>
+        <CameraView
+          style={{ flex: 1 }}
+          facing={facing}
+          enableTorch={isTorchOn}
+          onBarcodeScanned={scanned ? undefined : handleBarCodeScanned}
+          barcodeScannerSettings={{
+            barcodeTypes: [
+              "aztec",
+              "ean13",
+              "ean8",
+              "qr",
+              "pdf417",
+              "upc_e",
+              "datamatrix",
+              "code39",
+              "code93",
+              "itf14",
+              "codabar",
+              "code128",
+              "upc_a",
+            ],
+          }}
+        ></CameraView>
+        <ButtonPrimary
+          buttonProps={{
+            onPress: handleTorchButtonClick,
+          }}
+          title={"Toggle flashlight"}
+        ></ButtonPrimary>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+});
